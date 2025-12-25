92b5037fee2dd7cc88747b1caac2a2213638b5bf
Niko Raes <niko.raes@gmail.com>
2025-12-25 12:17:54 +0100

    Implement local Helm-like template functions for enhanced template processing
    - Replaced usage of sprig functions with a custom FuncMap in databasequeryresource_controller.go for parsing templates.
    - Added funcs.go to define additional template functions including YAML, TOML, and JSON conversions.
    - Ensured compatibility with Helm-like templating features by providing a comprehensive set of functions.

---
+++ internal/controller/funcs.go
@@ -0,0 +1,145 @@
+// Copyright The Helm Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package controller
+
+import (
+	bytes "bytes"
+	encoding_json "encoding/json"
+	"maps"
+	"strings"
+	"text/template"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Masterminds/sprig/v3"
+	"sigs.k8s.io/yaml"
+	goYaml "sigs.k8s.io/yaml/goyaml.v3"
+)
+
+// FuncMap returns a mapping of all of the functions that Helm Engine has.
+func FuncMap() template.FuncMap {
+	f := sprig.TxtFuncMap()
+	delete(f, "env")
+	delete(f, "expandenv")
+
+	extra := template.FuncMap{
+		"toToml":        toTOML,
+		"fromToml":      fromTOML,
+		"toYaml":        toYAML,
+		"mustToYaml":    mustToYAML,
+		"toYamlPretty":  toYAMLPretty,
+		"fromYaml":      fromYAML,
+		"fromYamlArray": fromYAMLArray,
+		"toJson":        toJSON,
+		"mustToJson":    mustToJSON,
+		"fromJson":      fromJSON,
+		"fromJsonArray": fromJSONArray,
+	}
+
+	maps.Copy(f, extra)
+	return f
+}
+
+func toYAML(v interface{}) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSuffix(string(data), "\n")
+}
+
+func mustToYAML(v interface{}) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return strings.TrimSuffix(string(data), "\n")
+}
+
+func toYAMLPretty(v interface{}) string {
+	var data bytes.Buffer
+	encoder := goYaml.NewEncoder(&data)
+	encoder.SetIndent(2)
+	err := encoder.Encode(v)
+	if err != nil {
+		return ""
+	}
+	return strings.TrimSuffix(data.String(), "\n")
+}
+
+func fromYAML(str string) map[string]interface{} {
+	m := map[string]interface{}{}
+	if err := yaml.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+func fromYAMLArray(str string) []interface{} {
+	a := []interface{}{}
+	if err := yaml.Unmarshal([]byte(str), &a); err != nil {
+		a = []interface{}{err.Error()}
+	}
+	return a
+}
+
+func toTOML(v interface{}) string {
+	b := bytes.NewBuffer(nil)
+	e := toml.NewEncoder(b)
+	err := e.Encode(v)
+	if err != nil {
+		return err.Error()
+	}
+	return b.String()
+}
+
+func fromTOML(str string) map[string]interface{} {
+	m := make(map[string]interface{})
+	if err := toml.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+func toJSON(v interface{}) string {
+	data, err := encoding_json.Marshal(v)
+	if err != nil {
+		return ""
+	}
+	return string(data)
+}
+
+func mustToJSON(v interface{}) string {
+	data, err := encoding_json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+func fromJSON(str string) map[string]interface{} {
+	m := make(map[string]interface{})
+	if err := encoding_json.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+func fromJSONArray(str string) []interface{} {
+	a := []interface{}{}
+	if err := encoding_json.Unmarshal([]byte(str), &a); err != nil {
+		a = []interface{}{err.Error()}
+	}
+	return a
+}
